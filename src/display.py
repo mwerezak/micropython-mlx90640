29e03244ab8516046618a5e54ec3eb549de238fe
Mike Werezak <mwerezak@gmail.com>
2022-07-07 22:25:52 -0400

    Implement pixel map display class

--- src/display.py
+++ src/display.py
@@ -1,3 +1,4 @@
+from ucollections import namedtuple
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_P8, PEN_RGB332, PEN_RGB565
 
 DISPLAY = PicoGraphics(display=DISPLAY_PICO_DISPLAY, pen_type=PEN_RGB332)
@@ -6,27 +7,40 @@ PEN_DEFAULT_BG = DISPLAY.create_pen(0, 0, 0)
 PEN_PIXMAP_0 = DISPLAY.create_pen(150, 200, 245)
 PEN_PIXMAP_1 = DISPLAY.create_pen(240, 240, 240)
 
-def draw_pixmap_dummy(display):
-    pix_colors = (PEN_PIXMAP_0, PEN_PIXMAP_1)
 
-    sensor_w, sensor_h = (24, 32)
-    display_w, display_h = display.get_bounds()
+Rect = namedtuple('Rect', ('x', 'y', 'width', 'height'))
 
-    pix_scale = min(display_w/sensor_w, display_h/sensor_h)
-    width, height = sensor_w*pix_scale, sensor_h*pix_scale
+class PixMap:
+    def __init__(self, width, height, buf):
+        # element size
+        self.width = width
+        self.height = height
+        if len(buf) != width * height:
+            raise ValueError(f"invalid buffer size for {width}x{height} PixMap: {len(buf)}")
 
-    origin_x = (display_w - width)/2.0
-    origin_y = (display_h - height)/2.0
-    square = int(round(pix_scale))
-    for j in range(sensor_h):
-        for i in range(sensor_w):
-            color_idx = (i+j) % len(pix_colors)
-            display.set_pen(pix_colors[color_idx])
-            
-            x = int(round(origin_x + i*pix_scale))
-            y = int(round(origin_y + j*pix_scale))
-            display.rectangle(x, y, square, square)
+        self.buf = buf  # an Array2D with data
+        self.draw_scale = 0 # size of element in pixels
+        self.draw_rect = Rect(0, 0, 0, 0)
 
-# class PixMap:
-#     def __init__(self, rect, bufsize):
-#         self.bufsize = bufsize
+    def update_rect(self, rect):
+        self.draw_scale = min(rect.width/self.width, rect.height/self.height)
+        draw_width = self.width * self.draw_scale
+        draw_height = self.height * self.draw_scale
+
+        # upper left corner
+        origin_x = (rect.width - draw_width)/2.0 + rect.x
+        origin_y = (rect.height - draw_height)/2.0 + rect.y
+        self.draw_rect = Rect(origin_x, origin_y, draw_width, draw_height)
+
+    def draw_dummy(self, display):
+        dummy_colors = (PEN_PIXMAP_0, PEN_PIXMAP_1)
+        square_size = int(round(self.draw_scale))
+        for j in range(self.height):
+            for i in range(self.width):
+                color_idx = (i+j) % len(dummy_colors)
+                display.set_pen(dummy_colors[color_idx])
+                
+                x = int(round(self.draw_rect.x + i*self.draw_scale))
+                y = int(round(self.draw_rect.y + j*self.draw_scale))
+                display.rectangle(x, y, square_size, square_size)
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from display import DISPLAY, PEN_DEFAULT_BG, draw_pixmap_dummy
+from array import array
+from pinmap import I2C_CAMERA
+from mlx90640 import detect_camera
+from mlx90640.calibration import NUM_ROWS, NUM_COLS
+from display import DISPLAY, PEN_DEFAULT_BG, PixMap, Rect
 
 DISPLAY.set_pen(PEN_DEFAULT_BG)
 DISPLAY.clear()
-draw_pixmap_dummy(DISPLAY)
-DISPLAY.update()
 
-from pinmap import I2C_CAMERA
-from mlx90640 import detect_camera
+buf = array('f', (0 for i in range(NUM_ROWS * NUM_COLS)))
+pixmap = PixMap(NUM_ROWS, NUM_COLS, buf)
+pixmap.update_rect(Rect(0, 0, *DISPLAY.get_bounds()))
+pixmap.draw_dummy(DISPLAY)
+DISPLAY.update()
 
 camera = detect_camera(I2C_CAMERA)
 im = camera.read_image()
